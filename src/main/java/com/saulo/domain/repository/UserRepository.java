6e77c2164bbd438b4a2311a6102552849b191dc8
Saulo <saulo.benedetti@hotmail.com>
2025-05-19 20:22:02 -0300

    Add core domain models and repository with configurations
    Introduce domain models including User, Account, Card, Feature, News, and InfoItem, along with a UserRepository for database interactions. Replace `application.properties` with `application-dev.yml` to configure H2 database and JPA settings for development. These additions establish the foundation for handling core application data.

---
+++ src/main/java/com/saulo/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.saulo.domain.repository;
+
+import com.saulo.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
